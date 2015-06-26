411541c65887729e907da09d1f5077febc34c549
joycezhou95 <joyce.zhou@berkeley.edu>
2015-06-25 22:42:59 -0700

    update site

--- home/assets/js/script.js
+++ home/assets/js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
-	function simpleParallax() {
-	    var scrolled = $(window).scrollTop() + 1;
+	// function simpleParallax() {
+	//     var scrolled = $(window).scrollTop() + 1;
 
-	    $('.scroll').css('background-position', '0' + -(scrolled * .1) + 'px');
-	}
+	//     $('.scroll').css('background-position', '0' + -(scrolled * .1) + 'px');
+	// }
 
-	$(window).scroll(function (e) {
-	    simpleParallax();
-	});
+	// $(window).scroll(function (e) {
+	//     simpleParallax();
+	// });
 
-	
+
 
 	$("#first-circle").click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#idk").offset().top - 100},
+	        scrollTop: $("#home").offset().top},
 	        'slow');
 	});
 
 	$("#second-circle").click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#idk-2").offset().top - 100},
+	        scrollTop: $("#about").offset().top},
 	        'slow');
 	});
 
